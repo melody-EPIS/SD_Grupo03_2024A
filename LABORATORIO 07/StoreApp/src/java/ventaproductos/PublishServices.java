beeaac21088007a70286411d70b97e8ad188b944
Melody Marisol Ramos Challa <64658009+melody-EPIS@users.noreply.github.com>
2024-06-11 09:51:40 -0500

    Update PublishServices.java
    Comentarios en la clase PublishServices

--- LABORATORIO 07/StoreApp/src/java/ventaproductos/PublishServices.java
+++ LABORATORIO 07/StoreApp/src/java/ventaproductos/PublishServices.java
@@ -1,10 +1,11 @@
-package ventaproductos;
+package ventaproductos; // Define el paquete donde se encuentra la clase
 
-import javax.xml.ws.Endpoint;
+import javax.xml.ws.Endpoint; // Importa la clase Endpoint del paquete javax.xml.ws
 
-public class PublishServices {
-    public static void main(String[] args) {
+public class PublishServices { // Define la clase PublishServices
+    public static void main(String[] args) { // Método principal
+        // Publica un servicio web en la dirección especificada con una implementación dada
         Endpoint.publish("http://localhost:1516/WS/Products", new SOAPImpl());
-        System.out.println("Service published at http://localhost:1516/WS/Products");
+        System.out.println("Service published at http://localhost:1516/WS/Products"); // Imprime un mensaje de confirmación
     }
 }
