5cb59a3d7fd87fdc34c843b599b6824783263705
Melody Marisol Ramos Challa <64658009+melody-EPIS@users.noreply.github.com>
2024-06-11 06:43:29 -0500

    Update SOAPI.java
    Se realizaron cambios en el entendimiento del programa

--- LABORATORIO 07/EJERCICIO_RESUELTO/WSSOAP/src/main/java/LabSD/SOAPI.java
+++ LABORATORIO 07/EJERCICIO_RESUELTO/WSSOAP/src/main/java/LabSD/SOAPI.java
@@ -1,16 +1,17 @@
-package LabSD;
-import java.util.List;
-import javax.jws.WebMethod;
-import javax.jws.WebService;
-import LabSD.User;
+package LabSD; // Define el paquete en el que se encuentra la interfaz.
 
-@WebService
-public interface SOAPI {
+import java.util.List; // Importa la clase List para manejar listas de objetos.
+import javax.jws.WebMethod; // Importa la anotación WebMethod para marcar métodos como operaciones web.
+import javax.jws.WebService; // Importa la anotación WebService para definir una interfaz como servicio web.
+import LabSD.User; // Importa la clase User que será utilizada en los métodos de la interfaz.
 
-	@WebMethod
-	public List<User> getUsers();
-	
-	@WebMethod
-	public void addUser(User user);
-	
+@WebService // Marca esta interfaz como un servicio web.
+public interface SOAPI { // Declara la interfaz del servicio web SOAPI.
+
+    @WebMethod // Indica que este método es una operación web que se puede invocar.
+    public List<User> getUsers(); // Define un método para obtener una lista de usuarios.
+    
+    @WebMethod // Indica que este método es una operación web que se puede invocar.
+    public void addUser(User user); // Define un método para añadir un usuario.
+    
 }
