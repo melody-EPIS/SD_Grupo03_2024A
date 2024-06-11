29a03ac573f498fbd4f76152d407097e42f8fe2c
Melody Marisol Ramos Challa <64658009+melody-EPIS@users.noreply.github.com>
2024-06-11 06:41:46 -0500

    Update PublishServices.java
    Se realizó cambios en la explicación del código.

--- LABORATORIO 07/EJERCICIO_RESUELTO/WSSOAP/src/main/java/LabSD/PublishServices.java
+++ LABORATORIO 07/EJERCICIO_RESUELTO/WSSOAP/src/main/java/LabSD/PublishServices.java
@@ -1,11 +1,15 @@
-package LabSD;
+package LabSD; // Define el paquete en el que se encuentra la clase.
 
-import javax.xml.ws.Endpoint;
-import javax.xml.*;
-import LabSD.SOAPImpl;
+import javax.xml.ws.Endpoint; // Importa la clase Endpoint para publicar el servicio web.
+import javax.xml.*; // Importa todas las clases en el paquete javax.xml.
+import LabSD.SOAPImpl; // Importa la implementación del servicio web SOAP.
 
-public class PublishServices {
-	public static void main(String[] args) {
-		Endpoint.publish("http://localhost:1516/WS/Users", new SOAPImpl());
-	}
+public class PublishServices { // Declara la clase PublishServices.
+    public static void main(String[] args) { // Define el método principal, punto de entrada de la aplicación.
+        Endpoint.publish("http://localhost:1516/WS/Users", new SOAPImpl()); 
+        // Publica el servicio web en la URL especificada. 
+        // "http://localhost:1516/WS/Users" es la dirección en la que el servicio estará disponible.
+        // new SOAPImpl() es la instancia de la implementación del servicio web que se publica.
+    }
 }
+
