88ec587604280435edeea7e7e5ea239c372059f1
Melody Marisol Ramos Challa <64658009+melody-EPIS@users.noreply.github.com>
2024-06-11 06:44:42 -0500

    Update SOAPImpl.java
    Propósito de cada línea de código

--- LABORATORIO 07/EJERCICIO_RESUELTO/WSSOAP/src/main/java/LabSD/SOAPImpl.java
+++ LABORATORIO 07/EJERCICIO_RESUELTO/WSSOAP/src/main/java/LabSD/SOAPImpl.java
@@ -1,16 +1,20 @@
-package LabSD;
+package LabSD; // Define el paquete en el que se encuentra la clase.
 
-import java.util.List;
-import javax.jws.WebService;
-import LabSD.User;
-@WebService(endpointInterface = "LabSD.SOAPI")
-public class SOAPImpl implements SOAPI {
-	@Override
-	public List<User> getUsers() {
-		return User.getUsers();
-	}
-	@Override
-	public void addUser(User user) {
-		User.getUsers().add(user);
-	}
+import java.util.List; // Importa la clase List para manejar listas de objetos.
+import javax.jws.WebService; // Importa la anotación WebService para marcar la clase como un servicio web.
+import LabSD.User; // Importa la clase User que será utilizada en los métodos de la clase.
+
+// Marca esta clase como una implementación de un servicio web y especifica la interfaz que implementa.
+@WebService(endpointInterface = "LabSD.SOAPI") 
+public class SOAPImpl implements SOAPI { // Declara la clase SOAPImpl que implementa la interfaz SOAPI.
+
+    @Override // Sobrescribe el método getUsers de la interfaz SOAPI.
+    public List<User> getUsers() { // Implementa el método para obtener una lista de usuarios.
+        return User.getUsers(); // Llama al método estático getUsers de la clase User para obtener la lista de usuarios.
+    }
+    
+    @Override // Sobrescribe el método addUser de la interfaz SOAPI.
+    public void addUser(User user) { // Implementa el método para añadir un usuario.
+        User.getUsers().add(user); // Añade el usuario a la lista de usuarios llamando al método estático getUsers de la clase User.
+    }
 }
